9786044dd4ba8bb36259f5bc87d23e4898129299
bhagath <bhagath@getcustomexposure.com>
2018-04-09 23:21:56 +0530

    recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ recipes: Recipe[] = [
+ 	new Recipe('A Test Recipe', 'This is simply a test', 'https://pixabay.com/get/ea37b8062bf4083ed1584d05fb1d4e97e07ee3d21cac104497f3c370a1ecb3bd_340.jpg')
 
+ ];
   constructor() { }
 
   ngOnInit() {
